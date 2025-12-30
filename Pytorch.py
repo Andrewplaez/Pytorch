4e09b44781002aadf6a72e0b7724804d9b1c3397
Andrew Eze <andreweze@Andrews-MacBook-Pro.local>
2025-12-30 19:06:51 +0100

    Adding tensors

--- Pytorch.py
+++ Pytorch.py
@@ -12,8 +12,20 @@ float_32_tensor = torch.tensor([3.0, 6.0, 9.0], # creating a tensor
 
 float_16_tensor = float_32_tensor.type(dtype= torch.float16) # turn the tensors to float 16
 int_32_tensor = torch.tensor([3, 6, 9],
-                             dtype = torch.int32,
+                             dtype = torch.int32, #intiger type
                              )
 
+# Create a tensor 
+random_tensor = torch.rand(3, 4)
 
-print(int_32_tensor * float_32_tensor) # multipilying two tensors
+
+#tensor operations
+#" Tensor operations include "
+# Addition & Subtraction
+# Multiplication & division
+
+# adding tensors
+ 
+Adding_Tensor = torch.tensor([1, 2, 3])
+
+print(Adding_Tensor + 10)
