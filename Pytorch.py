3ed637802d75e03d19394e1c5265ebbb4412a790
Andrew Eze <andreweze@Andrews-MacBook-Pro.local>
2025-12-31 09:36:10 +0100

    min max min sum

--- Pytorch.py
+++ Pytorch.py
@@ -28,7 +28,49 @@ random_tensor = torch.rand(3, 4)
  
 Adding_Tensor = torch.tensor([1, 2, 3])
 
+# dot product 
+matrix_1 = torch.tensor([[1, 2, 3],
+                         [3, 4, 5]])
+
+matrix_2 = torch.tensor([[6, 7, 8],
+                         [7, 9, 11]])
+
 print(Adding_Tensor + 10)
 
 # in built tensor functions 
 print(torch.mul(Adding_Tensor, 10))
+
+# Multiplying tensor
+print(torch.matmul(matrix_1, matrix_2.T))
+
+# rules for multiplying tensors
+# 1 : the inner dimensions must match 
+# 2 : the resulting matricx has the dimension of the outer matrix
+
+# Shapes for matrix multiplication
+#Create two Matrices 
+
+tensor_A = torch.tensor([[1, 2],
+                         [3, 4],
+                         [5, 6],])
+
+tensor_B = torch.tensor([[7, 10],
+                         [8, 11],
+                         [9, 12]])
+
+
+# to fix a tensor shape issues we can manipulate the shape of our tensor
+#by using transpose
+
+New_Tensor_B = tensor_B.T
+
+# min, max, mean, sum 
+
+torch.min(tensor_A) or tensor_A.min
+
+#find the mean 
+
+torch.mean(tensor_A.type(torch.float32))
+
+# find the sum
+torch.sum(tensor_A)
